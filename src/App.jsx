01b9db1b95016b72f47c567c9bf8a9d0c65de2e2
Edward <140621113+EddyingPrime@users.noreply.github.com>
2023-11-07 15:56:07 +0800

    1.2

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import "./App.css";
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
+  const [xScore, setXScore] = useState(0);
+  const [oScore, setOScore] = useState(0);
+  const [drawScore, setDrawScore] = useState(0);
 
   const handleClick = (index) => {
     const squares = [...board];
@@ -25,40 +28,69 @@ function App() {
     );
   };
 
-  //Result
+  // Result
   const winner = calculateWinner(board);
-  const status = winner
-    ? `Winner: ${winner}`
-    : `Next player: ${xIsNext ? "X" : "O"}`;
+
+  const handleGameEnd = () => {
+    if (winner) {
+      if (winner === "X") {
+        setXScore(xScore + 1);
+      } else {
+        setOScore(oScore + 1);
+      }
+    } else if (board.every((square) => square !== null)) {
+      // Draw
+      setDrawScore(drawScore + 1);
+    }
+  };
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
     setXIsNext(true);
+    handleGameEnd();
   };
 
-  //Gameboard & Reset button
+  const resetScores = () => {
+    setXScore(0);
+    setOScore(0);
+    setDrawScore(0);
+  };
+
+  // Gameboard & Reset buttons
   return (
     <>
       <div className="game">
         <div className="game-board">
           <div className="game-info">
-            <div className="reset">{status}</div>
+            <div className="reset">
+              {winner
+                ? `Winner: ${winner}`
+                : board.every((square) => square !== null)
+                ? "It's a draw!"
+                : `Next player: ${xIsNext ? "X" : "O"}`}
+            </div>
+            <div className="score">
+              X: {xScore} | O: {oScore} | Draw: {drawScore}
+            </div>
             <button onClick={resetGame}>Reset Game</button>
+            <button onClick={resetScores}>Reset Scores</button>
           </div>
-          <div className="board-row">
-            {renderSquare(0)}
-            {renderSquare(1)}
-            {renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {renderSquare(3)}
-            {renderSquare(4)}
-            {renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {renderSquare(6)}
-            {renderSquare(7)}
-            {renderSquare(8)}
+          <div className="gameplay">
+            <div className="board-row">
+              {renderSquare(0)}
+              {renderSquare(1)}
+              {renderSquare(2)}
+            </div>
+            <div className="board-row">
+              {renderSquare(3)}
+              {renderSquare(4)}
+              {renderSquare(5)}
+            </div>
+            <div className="board-row">
+              {renderSquare(6)}
+              {renderSquare(7)}
+              {renderSquare(8)}
+            </div>
           </div>
         </div>
       </div>
@@ -66,7 +98,7 @@ function App() {
   );
 }
 
-//Calulation based on "board-rows"
+// Calculation based on "board-rows"
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
